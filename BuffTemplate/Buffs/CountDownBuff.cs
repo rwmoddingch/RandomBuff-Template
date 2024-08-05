63088abe9c5e61c84aed31bacb1c9267d44c239b
Tricrystal <34974954+pkuyo@users.noreply.github.com>
2024-08-05 14:44:41 +0800

    添加项目文件。

---
+++ BuffTemplate/Buffs/CountDownBuff.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RandomBuff.Core.Buff;
+using RandomBuffUtils;
+
+namespace BuffTemplate.Buffs
+{
+
+    internal class CountDownBuffData : CountableBuffData
+    {
+        public override BuffID ID => BuffEnums.CountDownBuff;
+        public override int MaxCycleCount => 5;
+    }
+
+    internal class CountDownBuff : Buff<CountDownBuff,CountDownBuffData>
+    {
+        public override BuffID ID => BuffEnums.CountDownBuff;
+
+        public CountDownBuff()
+        {
+            if (BuffCustom.TryGetGame(out var game))
+            {
+                foreach (var player in game.Players.Select(i => i.realizedCreature as Player))
+                {
+                    if(player == null) continue;
+
+                    //这种更改玩家属性的方法有更好的兼容性
+                    player.slugcatStats.Modify(this, PlayerUtils.Multiply, "runspeedFac", 1.5f);            //  player.slugcatStats.runspeedFac *= 1.5f;
+                    player.slugcatStats.Modify(this, PlayerUtils.Multiply, "corridorClimbSpeedFac", 1.5f);  //  player.slugcatStats.corridorClimbSpeedFac *= 1.5f;
+                    player.slugcatStats.Modify(this, PlayerUtils.Multiply, "poleClimbSpeedFac", 1.5f);      //  player.slugcatStats.poleClimbSpeedFac *= 1.5f;
+
+                }
+            }
+        }
+
+        public override void Destroy()
+        {
+            base.Destroy();
+
+            //在删除时撤销所有对玩家属性的修改
+            PlayerUtils.UndoAll(this);
+        }
+    }
+
+    internal class CountDownBuffHook
+    {
+        public static void HookOn()
+        {
+            On.Player.ctor += Player_ctor;
+        }
+
+        private static void Player_ctor(On.Player.orig_ctor orig, Player self, AbstractCreature abstractCreature, World world)
+        {
+            orig(self, abstractCreature, world);
+            if (self.isNPC) return;
+            self.slugcatStats.Modify(CountDownBuff.Instance, PlayerUtils.Multiply, "runspeedFac", 1.5f);            //  player.slugcatStats.runspeedFac *= 1.5f;
+            self.slugcatStats.Modify(CountDownBuff.Instance, PlayerUtils.Multiply, "corridorClimbSpeedFac", 1.5f);  //  player.slugcatStats.corridorClimbSpeedFac *= 1.5f;
+            self.slugcatStats.Modify(CountDownBuff.Instance, PlayerUtils.Multiply, "poleClimbSpeedFac", 1.5f);      //  player.slugcatStats.poleClimbSpeedFac *= 1.5f;
+
+        }
+    }
+}
---
+++ BuffTemplate/Buffs/StackableBuff.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RandomBuff.Core.Buff;
+using RandomBuffUtils;
+using RWCustom;
+using UnityEngine;
+
+namespace BuffTemplate.Buffs
+{
+    internal class StackableBuffData : BuffData
+    {
+        public override BuffID ID => BuffEnums.StackableBuff;
+    }
+
+
+    internal class StackableBuff : Buff<StackableBuff,StackableBuffData>
+    {
+        public override BuffID ID => BuffEnums.StackableBuff;
+
+        public StackableBuff()
+        {
+            if (BuffCustom.TryGetGame(out var game))
+            {
+                foreach (var player in game.Players.Select(i => i.realizedCreature as Player))
+                {
+                    if(player == null || player.room == null)
+                        continue;
+
+                    player.room.AddObject(new SimpleShield(player.room, player, Data.StackLayer));
+                }
+            }
+        }
+    }
+
+    internal class StackableBuffHook
+    {
+        public static void HookOn()
+        {
+            On.Player.NewRoom += Player_NewRoom;
+        }
+
+        private static void Player_NewRoom(On.Player.orig_NewRoom orig, Player self, Room newRoom)
+        {
+            orig(self, newRoom);
+            newRoom.AddObject(new SimpleShield(newRoom, self, StackableBuff.Instance.Data.StackLayer));
+        }
+    }
+
+    internal class SimpleShield : CosmeticSprite
+    {
+        public SimpleShield(Room room,Player player, int stackLayer)
+        {
+            this.room = room;
+            this.player = player;
+            this.stackLayer = stackLayer + 2;
+        }
+
+
+        public override void InitiateSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
+        {
+            FContainer shieldContainer = new FContainer();
+            for (int i = 0; i < stackLayer; i++)
+            {
+                FSprite sprite = new FSprite("Futile_White");
+                sprite.scale = 0.5f;
+                sprite.SetPosition(Custom.DegToVec(i * 360f / stackLayer) * 70);
+                shieldContainer.AddChild(sprite);
+            }
+
+            sLeaser.sprites = Array.Empty<FSprite>();
+            
+            sLeaser.containers = new[] { shieldContainer };
+            AddToContainer(sLeaser, rCam, null);
+        }
+
+
+        public override void AddToContainer(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, FContainer newContatiner)
+        {
+            if (newContatiner == null)
+                newContatiner = rCam.ReturnFContainer("Foreground");
+            newContatiner.AddChild(sLeaser.containers[0]);
+        }
+
+        public override void DrawSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
+        {
+            base.DrawSprites(sLeaser, rCam, timeStacker, camPos);
+            sLeaser.containers[0].SetPosition(Vector2.Lerp(lastPos, pos, timeStacker) - camPos);
+            sLeaser.containers[0].rotation = Mathf.Lerp(lastRotation, rotation, timeStacker);
+        }
+
+        public override void Update(bool eu)
+        {
+            base.Update(eu);
+            pos = player.mainBodyChunk.pos;
+            lastPos = player.mainBodyChunk.lastPos;
+
+            lastRotation = rotation;
+            rotation += 0.25f * 360 / 40;
+
+            foreach (var crit in room.updateList.OfType<Creature>())
+            {
+                if(crit is Player) continue;
+                for (int i = 0; i < stackLayer; i++)
+                {
+                    if (crit.bodyChunks.Any(chunk =>
+                            Custom.DistLess(chunk.pos, pos + Custom.DegToVec((i * 360f / stackLayer) + rotation) * 70, 12)))
+                    {
+                        crit.Stun(50);
+                        break;
+                    }
+                }
+            }
+
+            if(StackableBuff.Instance == null || player.room != room)
+                Destroy();
+        }
+
+        private readonly Player player;
+        private readonly int stackLayer;
+
+        private float lastRotation;
+        private float rotation;
+
+    }
+}
